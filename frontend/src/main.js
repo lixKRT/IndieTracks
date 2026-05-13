2b8043328410c0f414a7437d224cff075fad3932
ZHANGE\c1949 <c1949166@qq.com>
2026-05-14 01:02:22 +0800

    Revert "第一次提交首页的实现代码"
    This reverts commit 1b8799360ba24b51ec612df918ccf50f8e5ae900.

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,27 +1,5 @@
-import { createApp } from 'vue';
-import { createRouter, createWebHistory } from 'vue-router';
-import App from './App.vue';
-import Home from './views/Home.vue';
-import Labels from './views/Labels.vue';
+import { createApp } from 'vue'
+import './style.css'
+import App from './App.vue'
 
-const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/labels',
-    name: 'Labels',
-    component: Labels
-  }
-];
-
-const router = createRouter({
-  history: createWebHistory(),
-  routes
-});
-
-const app = createApp(App);
-app.use(router);
-app.mount('#app');
+createApp(App).mount('#app')
